95c47258045dea212ca9f46608f769b1e27c1ae0
strangecodder <strangecodder@gmail.com>
2025-03-29 21:06:58 +0300

    init diploma project

---
+++ opcserver/src/test/java/com/opcserver/OpcserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opcserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpcserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
