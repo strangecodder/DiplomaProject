0b305e5b00a10d745b7e7c971c9adb66c748064d
strangecodder <strangecodder@gmail.com>
2025-04-30 10:05:11 +0300

    feat: добавил сервис для реализации взаимодействия с бд во время мониторинга

---
+++ backendservice/src/main/java/com/example/analyticservice/service/MonitoringService.java
@@ -0,0 +1,38 @@
+package com.example.analyticservice.service;
+
+import com.commondto.dto.SensorValueDTO;
+import com.example.analyticservice.entity.Machine;
+import com.example.analyticservice.entity.MachineData;
+import com.example.analyticservice.entity.MachineSensor;
+import com.example.analyticservice.entity.Sensor;
+import com.example.analyticservice.repository.MachineDataRepository;
+import com.example.analyticservice.repository.MachineSensorRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MonitoringService {
+
+    private final MachineDataRepository machineDataRepository;
+    private final MachineSensorRepository machineSensorRepository;
+
+    @Transactional
+    public void saveValue(SensorValueDTO sensorValueDTO) {
+        MachineData machineData = new MachineData();
+
+        //Sensor sensor = machineSensorRepository.findSensorBySensor_NodeId(sensorValueDTO.getNodeName());
+
+        Sensor sensor = machineSensorRepository.findMachineSensorBySensor_OpcUaServerAndSensor_NodeId(
+                sensorValueDTO.getServerUrl(),
+                sensorValueDTO.getNodeName()
+        ).getSensor();
+
+        machineData.setSensorId(sensor);
+        machineData.setDataType("Снятие показания с датчика");
+        machineData.setDataValue(sensorValueDTO.getValue());
+        machineData.setDataTime(sensorValueDTO.getTime());
+        machineDataRepository.save(machineData);
+    }
+}
