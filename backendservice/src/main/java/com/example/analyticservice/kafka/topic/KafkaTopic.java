cb25d55ec9a2237a6c79c088cd2cf5cb8154d480
strangecodder <strangecodder@gmail.com>
2025-04-30 10:08:06 +0300

    feat: созданы kafka топики

---
+++ backendservice/src/main/java/com/example/analyticservice/kafka/topic/KafkaTopic.java
@@ -0,0 +1,17 @@
+package com.example.analyticservice.kafka.topic;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopic {
+
+    @Bean
+    public NewTopic sensorValueTopic() {
+        return TopicBuilder.name("log-data").build();
+    }
+
+    //todo: добавить ещё топики, если надо?
+}
