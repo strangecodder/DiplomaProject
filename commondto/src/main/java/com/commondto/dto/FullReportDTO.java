6a2d02a198012368f8ba98d1c5f8899671a6fe9f
strangecodder <strangecodder@gmail.com>
2025-05-02 23:51:29 +0300

    feat: добавил поле code для соответсвия с мапой в listner

--- commondto/src/main/java/com/commondto/dto/FullReportDTO.java
+++ commondto/src/main/java/com/commondto/dto/FullReportDTO.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class FullReportDTO {
+    private Long code;
     private String username;
 
     private String machineName;
--- commondto/src/main/java/com/commondto/dto/ReportDTO.java
+++ commondto/src/main/java/com/commondto/dto/ReportDTO.java
@@ -6,6 +6,7 @@ import java.sql.Timestamp;
 
 @Data
 public class ReportDTO {
+    private Long code;
     private String username;
     private Long machineId;
     private Long sensorId;
