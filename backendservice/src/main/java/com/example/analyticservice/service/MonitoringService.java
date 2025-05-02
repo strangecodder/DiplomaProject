b35fbff538ef7234e5e4cb8c825086209c0d75a5
strangecodder <strangecodder@gmail.com>
2025-05-03 00:25:15 +0300

    feat: добавил

--- backendservice/src/main/java/com/example/analyticservice/service/MonitoringService.java
+++ backendservice/src/main/java/com/example/analyticservice/service/MonitoringService.java
@@ -1,12 +1,16 @@
 package com.example.analyticservice.service;
 
+import com.commondto.dto.FullReportDTO;
 import com.commondto.dto.SensorValueDTO;
+import com.example.analyticservice.dto.CreateMachineDTO;
 import com.example.analyticservice.entity.Machine;
 import com.example.analyticservice.entity.MachineData;
 import com.example.analyticservice.entity.MachineSensor;
 import com.example.analyticservice.entity.Sensor;
 import com.example.analyticservice.repository.MachineDataRepository;
+import com.example.analyticservice.repository.MachineRepository;
 import com.example.analyticservice.repository.MachineSensorRepository;
+import com.example.analyticservice.util.MachineState;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,6 +21,21 @@ public class MonitoringService {
 
     private final MachineDataRepository machineDataRepository;
     private final MachineSensorRepository machineSensorRepository;
+    private final MachineRepository machineRepository;
+
+    @Transactional
+    public void sendFullReport(FullReportDTO fullReportDTO){
+
+    }
+
+    @Transactional
+    public void saveMachine(CreateMachineDTO createMachineDTO){
+        Machine machine = new Machine();
+        machine.setName(createMachineDTO.getMachineName());
+        machine.setType(createMachineDTO.getMachineType());
+        machine.setState(MachineState.INACTIVE);
+        machineRepository.save(machine);
+    }
 
     @Transactional
     public void saveValue(SensorValueDTO sensorValueDTO) {
