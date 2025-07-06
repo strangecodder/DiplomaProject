428d58825d6fcbb6ea29b463712714ef95947692
strangecodder <strangecodder@gmail.com>
2025-07-06 14:39:32 +0300

    refactor: поправил оформление

--- opcservice/src/main/java/com/example/opcservice/config/WebSocketConfig.java
+++ opcservice/src/main/java/com/example/opcservice/config/WebSocketConfig.java
@@ -10,7 +10,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
-
         config.setApplicationDestinationPrefixes("/app");
     }
 
