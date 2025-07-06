1f12cafb9886da1c641557cc246c05ed81e20d56
strangecodder <strangecodder@gmail.com>
2025-07-06 14:35:35 +0300

    refactor: изменил название класса

--- backendservice/src/main/java/com/example/analyticservice/kafka/listener/KafkaListener.java
+++ backendservice/src/main/java/com/example/analyticservice/kafka/listener/KafkaListener.java
@@ -5,25 +5,23 @@ import com.commondto.dto.SensorValueDTO;
 import com.example.analyticservice.kafka.producer.KafkaFullReportSender;
 import com.example.analyticservice.service.MonitoringService;
 import com.example.analyticservice.service.ReportService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.KafkaListener;
 
 @Configuration
 @RequiredArgsConstructor
-public class KafkaOpcListener {
+public class KafkaListener {
 
     private final MonitoringService monitoringService;
     private final ReportService reportService;
     private final KafkaFullReportSender kafkaFullReportSender;
 
-    @KafkaListener(topics = "log-data", groupId = "group1",containerFactory = "kafkaListenerContainerFactory")
+    @org.springframework.kafka.annotation.KafkaListener(topics = "log-data", groupId = "group1",containerFactory = "kafkaListenerContainerFactory")
     void listener(SensorValueDTO sensorValueDTO) {
         monitoringService.saveValue(sensorValueDTO);
     }
 
-    @KafkaListener(topics = "data-report",groupId = "report_group",containerFactory = "kafkaListenerReportContainerFactory")
+    @org.springframework.kafka.annotation.KafkaListener(topics = "data-report",groupId = "report_group",containerFactory = "kafkaListenerReportContainerFactory")
     void reportListener(ReportDTO reportDTO) {
         kafkaFullReportSender.sendFullReport(reportService.createFullReport(reportDTO));
     }
