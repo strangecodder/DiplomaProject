c939cfdca78af8b84af5bd1fda87077ba8f6d439
strangecodder <strangecodder@gmail.com>
2025-07-06 14:36:33 +0300

    feat: добавил методы для взаимодействия с БД

--- backendservice/src/main/java/com/example/analyticservice/service/MonitoringService.java
+++ backendservice/src/main/java/com/example/analyticservice/service/MonitoringService.java
@@ -3,6 +3,7 @@ package com.example.analyticservice.service;
 import com.commondto.dto.FullReportDTO;
 import com.commondto.dto.SensorValueDTO;
 import com.example.analyticservice.dto.CreateMachineDTO;
+import com.example.analyticservice.dto.SensorConfigDTO;
 import com.example.analyticservice.entity.Machine;
 import com.example.analyticservice.entity.MachineData;
 import com.example.analyticservice.entity.MachineSensor;
@@ -10,6 +11,7 @@ import com.example.analyticservice.entity.Sensor;
 import com.example.analyticservice.repository.MachineDataRepository;
 import com.example.analyticservice.repository.MachineRepository;
 import com.example.analyticservice.repository.MachineSensorRepository;
+import com.example.analyticservice.repository.SensorRepository;
 import com.example.analyticservice.util.MachineState;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -22,11 +24,7 @@ public class MonitoringService {
     private final MachineDataRepository machineDataRepository;
     private final MachineSensorRepository machineSensorRepository;
     private final MachineRepository machineRepository;
-
-    @Transactional
-    public void sendFullReport(FullReportDTO fullReportDTO){
-
-    }
+    private final SensorRepository sensorRepository;
 
     @Transactional
     public void saveMachine(CreateMachineDTO createMachineDTO){
@@ -50,5 +48,27 @@ public class MonitoringService {
         machineData.setDataValue(sensorValueDTO.getValue());
         machineData.setDataTime(sensorValueDTO.getTime());
         machineDataRepository.save(machineData);
+    }
+
+    @Transactional
+    public void addNewSensorNode(SensorConfigDTO sensorConfigDTO) {
+        Sensor sensor = new Sensor();
+        sensor.setSensorName(sensorConfigDTO.getSensorName());
+        sensor.setSensorType(sensorConfigDTO.getSensorType());
+        sensor.setSensorDescription(sensorConfigDTO.getSensorDescription());
+        sensor.setMeasurementUnit(sensorConfigDTO.getMeasurementUnit());
+        sensor.setOpcUaServer(sensorConfigDTO.getUrl());
+        sensor.setNodeId(sensorConfigDTO.getNodeName());
+        sensor.setPeriod(sensorConfigDTO.getPeriod());
+        sensorRepository.save(sensor);
+        addToMachineSensor(sensor.getSensorId(),sensorConfigDTO.getMachineId());
+    }
+
+    @Transactional
+    public void addToMachineSensor(long sensorId, long machineId) {
+        MachineSensor machineSensor = new MachineSensor();
+        machineSensor.setSensor(sensorRepository.findById(sensorId).get());
+        machineSensor.setMachine(machineRepository.findById(machineId).get());
+        machineSensorRepository.save(machineSensor);
     }
 }
